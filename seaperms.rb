f59e56111b53d52bef844815aa922c3c11a809ae
Lalith <lalithr95@gmail.com>
2016-02-09 00:52:23 +0530

    Final code

--- seaperms.rb
+++ seaperms.rb
@@ -1,16 +1,15 @@
 #https://www.codechef.com/FEB16/problems/SEAPERMS
 N,D = gets.chomp.split(" ").map(&:to_i)
 A = gets.chomp.split(" ").map(&:to_i)
-M = gets.chomp.to_i
 PA = A.permutation.to_a
-
-def check_valid? perm, sum
+def check_valid_perm?(perm, sum)
 	flag = 1
-	perm.each_with_index do |x, i|
+	i = 0
+	perm.each_index do |i|
 		if i == (perm.count -1)
 			break
 		end
-		if A[perm[i]-1] + sum < A[perm[i+1]-1]
+		if (A[perm[i]-1] + sum) < A[perm[i+1]-1]
 			flag = 0
 			break
 		end
@@ -18,15 +17,19 @@ def check_valid? perm, sum
 	return flag
 end
 
-while M > 0 do
-	p,v = gets.chomp.split(" ").map(&:to_i)
-	count1 = 0
-	A[p-1] = v
-	PA.each do |perm|
-		if check_valid? perm, D
-			count1 += 1
+def main_meth
+	m = gets.chomp.to_i
+	while m > 0
+		p,v = gets.chomp.split(" ").map(&:to_i)
+		count1 = 0
+		A[p-1] = v
+		PA.each do |perm|
+			if check_valid_perm?(perm, D)
+				count1 += 1
+			end
 		end
+		puts count1
+		m -= 1
 	end
-	puts count1
-	M -= 1
 end
+main_meth
