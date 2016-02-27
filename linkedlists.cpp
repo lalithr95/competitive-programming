9ada2b8d287f8be35b66711b8d4d5d959a2b7a45
Lalith <lalithr95@gmail.com>
2016-02-28 00:03:56 +0530

    remove duplicate from list

--- linkedlists.cpp
+++ linkedlists.cpp
@@ -39,6 +39,45 @@ int Solution::lPalin(ListNode* A) {
 	return 1;
 }
 
+
+ListNode* Solution::deleteDuplicates(ListNode* A) {
+	ListNode *temp,*prev;
+	unordered_map<int, int> m;
+	temp = A;
+	prev = NULL;
+	while(temp!=NULL) {
+		if(m.find(temp->val) != m.end()) {
+			m[temp->val]++;
+		}
+		else {
+			m[temp->val] = 1;
+		}
+		temp = temp->next;
+	}
+	ListNode *head = NULL;
+	ListNode *temp2;
+	temp = A;
+	while(temp!=NULL) {
+		if(m[temp->val] == 1) {
+			if(head == NULL) {
+				temp2 = (ListNode *)malloc(1*sizeof(ListNode));
+				temp2->val = temp->val;
+				temp2->next = NULL;
+				head = temp2;
+			}
+			else {
+				temp2->next = (ListNode *)malloc(1*sizeof(ListNode));
+				temp2->next->val = temp->val;
+				temp2 = temp2->next;
+				temp2->next = NULL;
+			}
+		}
+		temp = temp->next;
+	}
+	return head;
+}
+
+
 int main() {
 	return 0;
 }
